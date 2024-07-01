1badc7668db236f198f06afdfb0fb894cb760ade
VishLella <vishlella.work@gmail.com>
2024-07-01 16:49:40 -0400

    added testing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    text = "This is a text node"
+    type = "bold"
+    url = "https://www.boot.dev"
+
+    node = TextNode(text, type, url)
+    #print(node)
+
+main()
